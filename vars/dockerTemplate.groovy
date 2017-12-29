bf7c4a449e87a2dd705eafb08096e41e9b236706
Andy Zhang <zch.ustc@gmail.com>
2017-12-29 23:12:06 +0800

    add sample lib

---
+++ vars/dockerTemplate.groovy
@@ -0,0 +1,8 @@
+#!/usr/bin/groovy
+def call(body) {
+  podTemplate(label: label,
+        containers: [containerTemplate(name: 'docker', image: 'docker', command: 'cat', ttyEnabled: true)],
+        volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
+    body()
+  }
+}
---
+++ vars/mavenTemplate.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/groovy
+def call(body) {
+  podTemplate(label: label,
+//        containers: [containerTemplate(name: 'maven', image: 'maven', command: 'cat', ttyEnabled: true)],
+//        volumes: [secretVolume(secretName: 'maven-settings', mountPath: '/root/.m2'),
+//                  persistentVolumeClaim(claimName: 'maven-local-repo', mountPath: '/root/.m2nrepo')]) {
+// FIXME: should add maven setting and better to add local repo as pv
+        containers: [containerTemplate(name: 'maven', image: 'maven:3.5-jdk-8-alpine', command: 'cat', ttyEnabled: true)]){
+          body()
+        }
+}
